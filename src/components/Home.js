4e12e8571c8b67a9d877eebc448938caf32b6e74
John <johnmulvey48@yahoo.com>
2022-07-01 10:18:48 -0400

    fixed home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 function Home() {
+    const homeStyle = {
+        height:"500px",
+        width:"500px"
+      };
     return (
         <div>
             <h1>
                 Welcome to Re-Blogger!
             </h1>
+            <div>
+                <form>
+                    <input placeholder="Username">
+                    </input>
+                    <input placeholder="Password">
+                    </input>
+                    <button>Login</button>
+                </form>
+                <img src="https://logos-world.net/wp-content/uploads/2021/08/Blogger-Logo-1999-2001.png" style={homeStyle}/>
+            </div>
         </div>
     )
 }
