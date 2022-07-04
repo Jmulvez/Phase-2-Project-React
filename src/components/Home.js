cbc34b5e3b0c458525015d34bf15fde24354022c
John <johnmulvey48@yahoo.com>
2022-07-04 08:45:21 -0400

    added login navigation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Home() {
+    let history = useHistory();
     const homeStyle = {
         height:"500px",
         width:"500px"
@@ -11,13 +13,12 @@ function Home() {
                 Welcome to Re-Blogger!
             </h1>
             <div>
-                Log in or Create Account
                 <form>
                     <input placeholder="Username">
                     </input>
                     <input placeholder="Password">
                     </input>
-                    <button>Login</button>
+                    <button onClick={() => {history.push('/blogs')}}>Login</button>
                 </form>
                 <img src="https://logos-world.net/wp-content/uploads/2021/08/Blogger-Logo-1999-2001.png" alt="Oh no!" style={homeStyle}/>
             </div>
